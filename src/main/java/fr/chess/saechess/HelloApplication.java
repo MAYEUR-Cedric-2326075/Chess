03b1ff49a4646c41efdd49216d71e6c13432eb98
cedric <cedric.mayeur@etu.univ-amu.fr>
2024-06-04 16:10:56 +0200

    mise en page de la page d'aceuille + logo

--- src/main/java/fr/chess/saechess/HelloApplication.java
+++ src/main/java/fr/chess/saechess/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("Chess_play");
         stage.getIcons().add(new Image("logo.png"));
         stage.setScene(scene);
