81267d2db07bc0d8e6a63f2ee3d8074f151c7316
cedric <cedric.mayeur@etu.univ-amu.fr>
2024-06-04 17:16:00 +0200

    les 2 timer dont fonctionel + set on action sur les 2 menu et le bouton jouer

--- src/main/java/fr/chess/saechess/HelloController.java
+++ src/main/java/fr/chess/saechess/HelloController.java
@@ -3,24 +3,50 @@ package fr.chess.saechess;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    private int secondsRemaining = 600; // 600 = 10 minutes    /     1800 = 30 min
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private SplitMenuButton menu1;
+    @FXML
+    private SplitMenuButton menu2;
+
+    @FXML
+    protected void pvp() {
+        menu1.setText("player vs player");
+    }
+    @FXML
+    protected void pve() {
+        menu1.setText("player vs bot");
     }
 
+    @FXML
+    protected void time1() {
+        menu2.setText("10 minutes");
+        secondsRemaining = 600; // 600 = 10 minutes
+    }
+    @FXML
+    protected void time2() {
+        menu2.setText("30 minutes");
+        secondsRemaining = 1800; // 1800 = 30 min
+    }
+
+    @FXML
+    protected void btnJouer() {
+        //lancer une partie
+    }
 
     @FXML
     private Label timerLabelN;
+    @FXML
     private Label timerLabelB;
-
-    private int secondsRemaining = 600; // 10 minutes
 
     public void initialize() {
         Timer timer = new Timer();
@@ -33,11 +59,14 @@ public class HelloController {
                         int minutes = secondsRemaining / 60;
                         int seconds = secondsRemaining % 60;
                         timerLabelN.setText(String.format("%02d:%02d", minutes, seconds));
+                        timerLabelB.setText(String.format("%02d:%02d", minutes, seconds));
                     } else {
                         timer.cancel();
                     }
                 });
             }
         }, 0, 1000);
+
+
     }
 }
