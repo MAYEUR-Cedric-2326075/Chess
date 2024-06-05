525f4fb9430dfbbb29ef9b4a019e3d5686864d94
zakharia <bensoltanez2004@gmail.com>
2024-06-05 12:00:10 +0200

    modifications d'u déplacemnt

--- src/main/java/fr/chess/saechess/HelloController.java
+++ src/main/java/fr/chess/saechess/HelloController.java
@@ -4,15 +4,17 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitMenuButton;
-
 import javafx.scene.image.ImageView;
-
 import java.util.Timer;
 import java.util.TimerTask;
+import javafx.scene.layout.GridPane;
+
 
 public class HelloController {
 
     private int secondsRemaining = 600; // 600 = 10 minutes / 1800 = 30 min
+    @FXML
+    private GridPane chessBoard; // Déclaration de la variable gridPane
 
     @FXML
     private SplitMenuButton menu1;
@@ -77,14 +79,22 @@ public class HelloController {
         }, 0, 1000);
         // Initialize the pawn qui est le pion
         initializePawn(pawn);
+
     }
+
     //déplacement des pion en maitenant le clique de la souris
     private void initializePawn(ImageView pawn) {
+        // Ajouter un écouteur d'événements de clic de souris pour déplacer le pion
         pawn.setOnMousePressed(event -> pawn.setOpacity(0.5));
         pawn.setOnMouseReleased(event -> pawn.setOpacity(1));
-        pawn.setOnMouseDragged(event -> {
-            pawn.setX(event.getX() - pawn.getFitWidth() / 2);
-            pawn.setY(event.getY() - pawn.getFitHeight() / 2);
+
+        pawn.setOnMouseClicked(event -> {
+            // Récupérer les coordonnées de la cellule de la grille où l'utilisateur a cliqué
+            int clickedColIndex = (int) (event.getX() / chessBoard.getWidth() * chessBoard.getColumnCount());
+            int clickedRowIndex = (int) (event.getY() / chessBoard.getHeight() * chessBoard.getRowCount());
+
+            // Déplacer le pion vers la cellule de la grille où l'utilisateur a cliqué
+            movePawnTo(clickedColIndex, clickedRowIndex);
         });
     }
 
@@ -92,4 +102,12 @@ public class HelloController {
 
 
 
+    private void movePawnTo(int colIndex, int rowIndex) {
+        GridPane.setConstraints(pawn, colIndex, rowIndex);
+    }
+
 }
+
+
+
+
