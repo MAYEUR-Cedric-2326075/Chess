37009d9a43c5cea53c7bae02454995c048af4ba0
zakharia <bensoltanez2004@gmail.com>
2024-06-05 09:17:04 +0200

    ajout de pion est de déplacemnt

--- src/main/java/fr/chess/saechess/HelloController.java
+++ src/main/java/fr/chess/saechess/HelloController.java
@@ -3,15 +3,16 @@ package fr.chess.saechess;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.shape.Circle;
+import javafx.scene.image.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class HelloController {
 
-    private int secondsRemaining = 600; // 600 = 10 minutes    /     1800 = 30 min
+    private int secondsRemaining = 600; // 600 = 10 minutes / 1800 = 30 min
 
     @FXML
     private SplitMenuButton menu1;
@@ -19,9 +20,18 @@ public class HelloController {
     private SplitMenuButton menu2;
 
     @FXML
+    private Label timerLabelN;
+    @FXML
+    private Label timerLabelB;
+
+    @FXML
+    private ImageView pawn;//image du pions
+
+    @FXML
     protected void pvp() {
         menu1.setText("player vs player");
     }
+
     @FXML
     protected void pve() {
         menu1.setText("player vs bot");
@@ -32,6 +42,7 @@ public class HelloController {
         menu2.setText("10 minutes");
         secondsRemaining = 600; // 600 = 10 minutes
     }
+
     @FXML
     protected void time2() {
         menu2.setText("30 minutes");
@@ -40,15 +51,13 @@ public class HelloController {
 
     @FXML
     protected void btnJouer() {
-        //lancer une partie
+        // lancer une partie
     }
 
-    @FXML
-    private Label timerLabelN;
-    @FXML
-    private Label timerLabelB;
 
+    @FXML
     public void initialize() {
+        // Initialize the timer
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -66,7 +75,19 @@ public class HelloController {
                 });
             }
         }, 0, 1000);
-
-
+        // Initialize the pawn
+        initializePawn(pawn);
     }
+    private void initializePawn(ImageView pawn) {
+        pawn.setOnMousePressed(event -> pawn.setOpacity(0.5));
+        pawn.setOnMouseReleased(event -> pawn.setOpacity(1));
+        pawn.setOnMouseDragged(event -> {
+            pawn.setX(event.getX() - pawn.getFitWidth() / 2);
+            pawn.setY(event.getY() - pawn.getFitHeight() / 2);
+        });
+    }
+
+
+
+
 }
