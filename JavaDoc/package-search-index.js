9498e1afa14f3263bb7c1faeefc30d9e9dcdca3e
MAYEUR-Cedric-2326075 <144834325+MAYEUR-Cedric-2326075@users.noreply.github.com>
2024-06-09 13:24:22 +0200

    ajout de la JavaDoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"fr.chess.saechess","l":"fr.chess.saechess"}];updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fr.chess.saechess","l":"ChessApplication"},{"p":"fr.chess.saechess","l":"ChessController"}];updateSearchResults();
